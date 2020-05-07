3d8f50670653b5f671bbaf65b9ff09b5ff5efce1
kalene APIOU <apiou.kalene@gmail.com>
2020-05-07 15:55:51 +0200

    add musees.html

--- src/js/SlideShow.js
+++ src/js/SlideShow.js
@@ -14,10 +14,14 @@ function currentSlide(n) {
 function showSlides(n) {
 	var i;
 	var slides = document.getElementsByClassName("mySlides");
+
+
+
 	var dots = document.getElementsByClassName("dot");
+
 	if (n > slides.length) { slideIndex = 1 }
 	if (n < 1) { slideIndex = slides.length }
-	
+
 	for (i = 0; i < slides.length; i++) {
 		slides[i].style.display = "none";
 	}
@@ -26,4 +30,10 @@ function showSlides(n) {
 	}
 	slides[slideIndex - 1].style.display = "block";
 	dots[slideIndex - 1].className += " active";
+
+	var list = document.getElementsByClassName("slideshow-container");
+	list.getElementsByClassName("child")[0].innerHTML = "Milk";
+
+	console.log(c)
+
 }
